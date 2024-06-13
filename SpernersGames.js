98f6eb17e71a62e5375a16374045a035e580f95a
ellis-coldren <catherine.e.coldren@gmail.com>
2024-06-13 13:00:04 -0400

    Merge branch 'main' of https://github.com/ellis-coldren/blog-test and animate andrew

--- SpernersGames.js
+++ SpernersGames.js
@@ -1,5 +1,7 @@
-var c = document.getElementById("TriangleCanvas");
-var ctx = c.getContext("2d");
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
-ctx.stroke();
+document.addEventListener("DOMContentLoaded", function() {
+    var c = document.getElementById("TriangleCanvas");
+    var ctx = c.getContext("2d");
+    ctx.moveTo(0, 0);
+    ctx.lineTo(200, 100);
+    ctx.stroke();
+});
