d63f23d49c6b5ffce5db4efb617c1ae79d5e338d
ellis-coldren <catherine.e.coldren@gmail.com>
2024-06-25 13:01:33 -0400

    trying to make navbarscroll

--- SpernersGames.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var c = document.getElementById("TriangleCanvas");
-    var ctx = c.getContext("2d");
-    ctx.moveTo(0, 0);
-    ctx.lineTo(200, 100);
-    ctx.stroke();
-});
