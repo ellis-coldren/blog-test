d1a1e85526d5a35bc1145992e36093985e5e36d5
ellis-coldren <catherine.e.coldren@gmail.com>
2024-08-23 11:32:22 -0400

    updating sperners lemma

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './styles.css'
+import './App.css';
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ThanksforVisiting from './pages/ThanksforVisiting'
+import SpernersLemma from './pages/SpernersLemma'
 function App() {
   return (
     <Router>
       <Routes>
         <Route path = "/" element={<HomePage/>}/>
+        <Route path = "/SpernersLemma" element = {<SpernersLemma/>}/>
         <Route path = "/ThanksforVisiting" element = {<ThanksforVisiting/>}/>
       </Routes>
         
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,16 +5,14 @@ import Logo from '../images/Logo.PNG';
 function NavBar() {
     return(
       <nav id="nav">
-        <div className="container">
-            <img src={Logo} className="logo" alt="Logo" />
-            <ul className="nav-list">
-              <li className="active"><Link to="/">Home</Link></li>
-              <li><a href="https://www.linkedin.com/in/catherine-ellis-coldren-a003b9196" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-              <li><Link to="/sperners-lemma">Sperner's Lemma</Link></li>
-              <li><Link to="/derham-cohomology">DeRham Cohomology</Link></li>
-              <li><Link to="/ThanksforVisiting">Thank you &#9829;</Link></li>
-            </ul>
-        </div>
+        <img src={Logo} className="logo" alt="Logo" />
+        <ul className="nav-list">
+          <li className="active"><Link to="/">Home</Link></li>
+          <li><a href="https://www.linkedin.com/in/catherine-ellis-coldren-a003b9196" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+          <li><Link to="/SpernersLemma">Sperner's Lemma</Link></li>
+          <li><Link to="/derham-cohomology">DeRham Cohomology</Link></li>
+          <li><Link to="/ThanksforVisiting">Thank you &#9829;</Link></li>
+        </ul>
       </nav>
     );
 }
