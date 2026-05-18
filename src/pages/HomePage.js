fec90637c3b9499550e48eca83d631783d03a4b2
ellis-coldren <catherine.e.coldren@gmail.com>
2026-05-18 18:52:21 -0400

    remove song and mobile fix

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -13,12 +13,12 @@ function HomePage() {
           <NavBar/>
         </header>
         <main>
-          <div class="homepage-row">
-            <div class="intro-left">
+          <div className="homepage-row">
+            <div className="intro-left">
               <img src={Dog}/>
             </div>
-            <div class = "intro-right">
-              <img src={Logo} class='center'/>
+            <div className = "intro-right">
+              <img src={Logo} className='center'/>
               
               <h3>I'm Ellis, a recent graduate of Boston University with a passion for problem solving. <br/>
               I studied math and computer science and focused on topology and geometry processing. <br/> 
@@ -27,24 +27,24 @@ function HomePage() {
             <hr/>
           </div>
 
-          <div class="homepage-row">
-            <div class= "project-item">
-              <img src={SLThumbnail} class='center'/>
-              <p class='desc'> An interactive UI to demonstrate the proof and applications of Sperner's Lemma, made in Unity</p>
+          <div className="homepage-row">
+            <div className= "project-item">
+              <img src={SLThumbnail} className='center'/>
+              <p className='desc'> An interactive UI to demonstrate the proof and applications of Sperner's Lemma, made in Unity</p>
               <ul className="nav-list">
                 <li><Link to="/SpernersLemma">Sperner's Lemma</Link></li>
               </ul>
             </div>
-            <div class= "project-item">
-              <img src={DCThumbnail} class='center'/>
-              <p class='desc'> An expository paper on the relationship between the homology and deRham cohomology groups of smooth manifolds, advised by Professor Takashi Kimura</p>
+            <div className= "project-item">
+              <img src={DCThumbnail} className='center'/>
+              <p className='desc'> An expository paper on the relationship between the homology and deRham cohomology groups of smooth manifolds, advised by Professor Takashi Kimura</p>
               <ul className="nav-list">
                 <li><Link to="/DeRham">DeRham Cohomology</Link></li>
               </ul>
             </div>
-            <div class= "project-item">
-              <img src={WNThumbnail} class='center'/>
-              <p class='desc'> A paper on how the winding number can be used to segment sketches. </p>
+            <div className= "project-item">
+              <img src={WNThumbnail} className='center'/>
+              <p className='desc'> A paper on how the winding number can be used to segment sketches. </p>
               <ul className="nav-list">
                 <li><a href="http://dx.doi.org/10.1111/cgf.15141" target="_blank" rel="noopener noreferrer">Winding Number</a></li>
               </ul>
--- src/pages/ThanksforVisiting.js
+++ src/pages/ThanksforVisiting.js
@@ -27,7 +27,6 @@ function ThanksforVisiting() {
                   <div><img src={imtheboss} height='170' width='170' class="rotate-90"/></div>
               </div>
           </section>
-          <iframe width='100%' height='300' scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1814779140&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div><a href="https://soundcloud.com/metroboomin" title="Metro Boomin" target="_blank">Metro Boomin</a> · <a href="https://soundcloud.com/metroboomin/bbl-drizzy-bpm-150-mp3" title="BBL DRIZZY BPM 150.mp3" target="_blank">BBL DRIZZY BPM 150.mp3</a></div>
         </main>
       </div>
   );
